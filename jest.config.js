f23d343a77c92c75d2481bb6fd3c6ea76b2a450c
vpapazian <vpapazian@slack-corp.com>
2020-07-26 00:41:11 -0700

    Add jest and dummy file/test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+export default {
+  roots: [
+    "<rootDir>/src"
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/src/popup"
+  ],
+}
---
+++ src/helloworld.test.ts
@@ -0,0 +1,9 @@
+import { sum } from './helloworld';
+
+test('basic', () => {
+  expect(sum()).toBe(0);
+});
+
+test('basic again', () => {
+  expect(sum(1, 2)).toBe(3);
+});
