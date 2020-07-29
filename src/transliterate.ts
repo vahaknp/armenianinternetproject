86ead85b33dbb62c559f09e34391f3536005ad5f
vpapazian <vpapazian@slack-corp.com>
2020-07-28 22:42:32 -0700

    Add transliterate layer

---
+++ src/transliterate.ts
@@ -0,0 +1,21 @@
+const _ = require('lodash'); // TODO: use import, import what you need.
+
+import * as armenianBasicMap from './letter_maps/english_to_armenian_basic';
+
+export function basicArmenianTransform(toTransform: string) {
+  let transformed = toTransform;
+
+  _.forEach(armenianBasicMap.englishToArmenianTriplesBasic, (replace, find) => {
+    transformed = _.replace(transformed, new RegExp(find, 'g'), replace);
+  });
+
+  _.forEach(armenianBasicMap.englishToArmenianDoublesBasic, (replace, find) => {
+    transformed = _.replace(transformed, new RegExp(find, 'g'), replace);
+  });
+
+  _.forEach(armenianBasicMap.englishToArmenianBasic, (replace, find) => {
+    transformed = _.replace(transformed, new RegExp(find, 'g'), replace);
+  });
+
+  return transformed;
+}
