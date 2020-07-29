719835db3fa787f19d99a196c63e88c0e6d3f831
vpapazian <vpapazian@slack-corp.com>
2020-07-28 22:42:09 -0700

    Remove test files

--- src/helloworld.test.ts
+++
@@ -1,9 +0,0 @@
-import { basicArmenianTransform } from './helloworld';
-
-test('basic transform', () => {
-  expect(basicArmenianTransform('a')).toBe('ա');
-});
-
-test('basic transform with undefined letter', () => {
-  expect(basicArmenianTransform('ա')).toBe(undefined);
-});
--- src/helloworld.ts
+++
@@ -1,5 +0,0 @@
-import { englishToArmenianBasic } from './letter_maps/english_to_armenian_basic';
-
-export function basicArmenianTransform(toTransform: string) {
-  return englishToArmenianBasic[toTransform];
-}
