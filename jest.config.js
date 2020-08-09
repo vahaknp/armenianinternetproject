fb45b4dfd3831e0450f56036c0e1b5bc056bc9a1
vpapazian <vpapazian@slack-corp.com>
2020-08-08 18:16:27 -0700

    Wip to fix tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ export default {
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.js': '<rootDir>/node_modules/babel-jest',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/src/popup', 'node_modules/?!(react-icons)'],
+  testPathIgnorePatterns: ['/node_modules/', '/src/popup', '/node_modules/(?!lodash-es)'],
 };
